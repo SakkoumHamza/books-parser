304287ad704f2d65708eb8226aa074be4fb4e871
sakkoumhamza <hamzasakkoum1@gmail.com>
2025-09-20 22:54:29 +0100

    Added a minimized GoodReads HTML template

---
+++ main_test.go
@@ -0,0 +1,22 @@
+package main
+
+const HTML = `
+<div id="BookPage__leftColumn">
+	<img class="ResponsiveImage" role="presentation" src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1612238791i/56916837.jpg" loading="eager" onerror="this.onerror=null;this.src='https://dryofg8nmyqjw.cloudfront.net/images/no-cover.png';">
+</div> 
+
+<div class="BookPage__rightColumn>
+	<div class="BookPageTitleSection" >
+		<h1 class="Text Text__title1" data-testid="bookTitle" aria-label="Book title: To Kill a Mockingbird">To Kill a Mockingbird</h1>
+	</div>
+	<div class="ContributorLinksList">
+		<span class="ContributorLink__name" data-testid="name">Harper Lee</span>
+	</div>
+	<div class="RatingStatistics__rating">
+		4.26
+	</div>
+	<div class="DetailsLayoutRightParagraph__widthConstrained">
+		<span class="Formatted">One of the best-loved stories of all time, <i>To Kill a Mockingbird</i> has been translated into more than forty languages, sold more than forty million copies worldwide, served as the basis for an enormously popular motion picture, and was voted one of the best novels of the twentieth century by librarians across the country. A gripping, heart-wrenching, and wholly remarkable coming-of-age tale in a South poisoned by virulent prejudice, it views a world of great beauty and savage iniquities through the eyes of a young girl, as her father — a crusading local lawyer — risks everything to defend a black man unjustly accused of a terrible crime.</span>
+	</div>
+</div>
+`
